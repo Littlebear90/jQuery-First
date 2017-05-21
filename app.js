32900df4285c40cf7f91e8dd6f9513ca4e79b0b2
Littlebear90 <dbh2010@yahoo.com>
2017-05-21 01:14:31 -0500

    add comments

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// The main function controls the events of the buttons
 var main = function(){
 	$("#catch-button").click("catch", animateButton);
 	$("#dont-button").mouseenter("dont", animateButton);
 }
+
+// The animateButton function determines the button
+//  that had its event triggered and then does the
+//  animation on the button
 var animateButton = function(button){
 	if (button.data == "catch"){
 		var button = $("#catch-button");
@@ -10,10 +15,10 @@ var animateButton = function(button){
 		var button = $("#dont-button");
 	}
 	button.animate({
-		left:Math.random()*500
+		left:Math.random()*500 //the distance drawn is determined here
 	},{ duration: 200, queue: false });
 	button.animate({
-		top:Math.random()*500
+		top:Math.random()*500 //the distance drawn is determined here
 	},{ duration: 200, queue: false });	
 };
 $(document).ready(main);
